3b1d212569f3f464a17bbe3824b207f9194e1c23
free <devqj@qq.com>
2018-07-28 15:50:46 +0800

    增加工具

---
+++ CSprojectReferenceSwitchTool/CSprojectReferenceSwitchTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSprojectReferenceSwitchTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Tools
+
+## CSprojectReferenceSwitchTool
+
+切换目录下的工程引用，从本地目录工程引用切换为Nuget引用
