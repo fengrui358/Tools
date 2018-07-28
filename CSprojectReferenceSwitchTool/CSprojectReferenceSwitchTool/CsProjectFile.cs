dd45f48a02ee591ec1202823f89c08458b81445c
free <devqj@qq.com>
2018-07-28 17:29:25 +0800

    获取所有的工程文件

---
+++ CSprojectReferenceSwitchTool/CSprojectReferenceSwitchTool/CsProjectFile.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace CSprojectReferenceSwitchTool
+{
+    public class CsProjectFile
+    {
+        public FileInfo FileInfo { get; private set; }
+
+        public CsProjectFile(FileInfo fileInfo)
+        {
+            FileInfo = fileInfo;
+        }
+    }
+}
--- CSprojectReferenceSwitchTool/CSprojectReferenceSwitchTool/Program.cs
+++ CSprojectReferenceSwitchTool/CSprojectReferenceSwitchTool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace CSprojectReferenceSwitchTool
 {
@@ -6,7 +8,21 @@ namespace CSprojectReferenceSwitchTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var baseDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            var allCsprojectsFiles = baseDir.GetFiles("*.csproj", SearchOption.AllDirectories).ToList();
+
+            if (args.Any())
+            {
+                foreach (var arg in args)
+                {
+                    var dir = new DirectoryInfo(arg);
+
+                    if (dir.Exists)
+                    {
+                        allCsprojectsFiles = allCsprojectsFiles.Where(s => !s.FullName.Contains(dir.FullName)).ToList();
+                    }
+                }
+            }
         }
     }
 }
