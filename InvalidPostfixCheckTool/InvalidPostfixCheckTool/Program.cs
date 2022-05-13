d85cb28399a2b0b1bad72c81f9cb10645837b4d4
fengrui <fengrui358@163.com>
2022-05-13 18:37:52 +0800

    后缀检验小工具

---
+++ InvalidPostfixCheckTool/InvalidPostfixCheckTool/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("开始查找");
+var sw = Stopwatch.StartNew();
+
+var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories);
+
+var regex = new Regex(@".*(\(\s*\d+\s*\)|（\s*\d+\s*）)\.");
+Parallel.ForEach(files, file =>
+{
+    if (regex.IsMatch(file))
+    {
+        Console.WriteLine(file);
+    }
+});
+
+sw.Stop();
+Console.WriteLine("查找结束，耗时：{0}", sw.ElapsedMilliseconds);
+
+Console.ReadLine();
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## CSprojectReferenceSwitchTool
 
 切换目录下的工程引用，从本地目录工程引用切换为Nuget引用
+
+## InvalidPostfixCheckTool
+
+后缀校验，用于检查后缀是否含有无效的数字字符
